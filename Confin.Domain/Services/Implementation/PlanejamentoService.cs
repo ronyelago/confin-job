3336d69c68c474939bc460efa1f7aecb1ad87b93
Ronye Rocha <ronye.rocha@gmail.com>
2023-10-18 11:00:33 -0300

    planejamentoService

--- Confin.Domain/Services/Implementation/PlanejamentoService.cs
+++ Confin.Domain/Services/Implementation/PlanejamentoService.cs
@@ -2,10 +2,3 @@ using Confin.Domain.Services.Interfaces;
 
 namespace Confin.Domain.Services.Implementation;
 
-public class PlanejamentoService : IPlanejamentoService
-{
-    public async Task GerarPlanejamento()
-    {
-        
-    }
-}
---
+++ Confin.Service/Services/PlanejamentoService.cs
@@ -0,0 +1,11 @@
+using Confin.Domain.Services.Interfaces;
+
+namespace Confin.Service.Services;
+
+public class PlanejamentoService : IPlanejamentoService
+{
+    public async Task GerarPlanejamento()
+    {
+        
+    }
+}
